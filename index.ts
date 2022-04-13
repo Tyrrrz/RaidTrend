5159572d84a2b20495dd8ad0ff92cd742ceff7be
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-04-13 16:08:41 +0000

    Add grouped output

--- index.ts
+++ index.ts
@@ -1,37 +1,33 @@
 import { getMessages } from './telegram';
 import { writeFile } from 'fs/promises';
 
-interface Alert {
-  area: string;
-  start: Date;
-  stop: Date;
-}
-
-const main = async () => {
-  const alerts: Alert[] = [];
+const getAlerts = async () => {
+  const alerts = [];
   const areaAlertStarts = new Map<string, Date>();
 
   for await (const message of getMessages('air_alert_ua')) {
     console.log('Processing message', message);
 
-    const status =
-      message.text.indexOf('Повітряна тривога') >= 0
-        ? 'start'
-        : message.text.indexOf('Відбій тривоги') >= 0
-        ? 'stop'
-        : null;
+    const status = message.text.includes('Повітряна тривога')
+      ? 'start'
+      : message.text.includes('Відбій тривоги')
+      ? 'stop'
+      : null;
 
     if (!status) {
       console.log('Unknown status');
       continue;
     }
 
-    const areas = RegExp('(?:(?:Повітряна тривога)|(?:Відбій тривоги)) в (.+)', 'i')
+    const areas = RegExp(
+      '(?:(?:Повітряна тривога)|(?:Відбій тривоги)) в (?:м\\. )?([^\\.]+?)(?:\\.|\n)',
+      'i'
+    )
       .exec(message.text)?.[1]
       ?.split(' та ')
       .map((s) => s.trim());
 
-    if (!areas) {
+    if (!areas || areas.length <= 0) {
       console.log('Unknown areas');
       continue;
     }
@@ -65,9 +61,29 @@ const main = async () => {
   }
 
   console.log(`Processed ${alerts.length} alerts`);
+  return alerts;
+};
 
+const main = async () => {
+  const alerts = await getAlerts();
+
+  // Save raw data
   await writeFile('output.json', JSON.stringify(alerts, null, 2));
   console.log('Saved data to output.json');
+
+  // Save grouped data
+  const alertGroups = [...new Set(alerts.map((alert) => alert.area))]
+    .map((area) => {
+      const alertsForArea = alerts
+        .filter((alert) => alert.area === area)
+        .map((alert) => ({ start: alert.start, stop: alert.stop }));
+
+      return { area, alerts: alertsForArea };
+    })
+    .sort((a, b) => a.area.localeCompare(b.area));
+
+  await writeFile('output-grouped.json', JSON.stringify(alertGroups, null, 2));
+  console.log('Saved grouped data to output-grouped.json');
 };
 
 main().catch((err) => console.error('Error', err));
--- readme.md
+++ readme.md
@@ -17,6 +17,8 @@ You can download the data from the artifacts associated with the [latest run](ht
 
 The format of data is as follows:
 
+- `output.json`
+
 ```json
 [
   {
@@ -25,7 +27,7 @@ The format of data is as follows:
     "stop": "2022-04-13T12:22:14.000Z"
   },
   {
-    "area": "м. Конотоп",
+    "area": "Конотоп",
     "start": "2022-04-13T11:55:29.000Z",
     "stop": "2022-04-13T12:22:26.000Z"
   },
@@ -33,6 +35,39 @@ The format of data is as follows:
     "area": "Чернігівська область",
     "start": "2022-04-13T06:00:47.000Z",
     "stop": "2022-04-13T13:01:50.000Z"
+  }
+]
+```
+
+- `output-grouped.json`
+
+```json
+[
+  {
+    "area": "Прилуцький район",
+    "alerts": [
+      {
+        "start": "2022-03-15T16:54:58.000Z",
+        "stop": "2022-03-15T17:02:43.000Z"
+      }
+    ]
+  },
+  {
+    "area": "Ніжинський район",
+    "alerts": [
+      {
+        "start": "2022-03-15T16:54:51.000Z",
+        "stop": "2022-03-15T17:02:45.000Z"
+      },
+      {
+        "start": "2022-03-22T06:38:12.000Z",
+        "stop": "2022-03-22T07:11:57.000Z"
+      },
+      {
+        "start": "2022-03-22T10:20:45.000Z",
+        "stop": "2022-03-22T12:52:23.000Z"
+      }
+    ]
   }
 ]
 ```
